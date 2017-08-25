69e96cf63e2eacde10523eb310d7b5df47024ef4
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-25 15:37:22 +0300

    refactor SERVER.js with async-await

--- server/server.js
+++ server/server.js
@@ -127,45 +127,40 @@ app.patch('/todos/:id', authenticate, (req, res)=>{
 })
 
 // destroy
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id
   if(!ObjectID.isValid(id)) {
     return res.status(400).send('Bad request')
   }
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  })
-    .then((todo) => {
-      if(!todo) {
-        return res.status(404).send('Not found')
-      }
-      res.status(200).send({todo})
+  try {
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
     })
-    .catch((err) => {
-      if (NODE_ENV == 'development') {console.log(err.message)}
-      res.status(500).send()
-    })
+    if(!todo) {
+      return res.status(404).send('Not found')
+    }
+    res.status(200).send({todo})
+  } catch (err) {
+    if (NODE_ENV == 'development') console.log(err.message)
+    res.status(500).send()
+  }
 })
 
 // users routes ------------------------------------------------------
 //create
-app.post('/users', (req, res) => {
-  let body = _.pick(req.body, ['email', 'password']),
+app.post('/users', async (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']),
     user = new User(body)
 
-
-  user.save()
-    .then(() => {
-      return user.generateAuthToken()
-    })
-    .then((token) => {
-      res.header('x-auth', token).send(user)
-    })
-    .catch((err) => {
-      if (NODE_ENV == 'development') {console.log(err.message)}
-      res.status(400).send()
-    })
+  try {
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.header('x-auth', token).send(user)
+  } catch (err) {
+    if (NODE_ENV == 'development') console.log(err.message)
+    res.status(400).send()
+  }
 })
 
 
@@ -175,30 +170,26 @@ app.get('/users/me', authenticate, (req, res) => {
 })
 
 //POST /users/login {email, password}
-app.post('/users/login', (req, res) => {
-  let body = _.pick(req.body, ['email', 'password'])
-  User.findByCredentials(body.email, body.password)
-    .then((user) => {
-       return user.generateAuthToken()
-         .then((token) => {
-           res.header('x-auth', token).send(user)
-         })
-    })
-    .catch((err) => {
-      if (NODE_ENV == 'development') {console.log(err.message)}
-      res.status(400).send()
-    })
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password'])
+    const user = await User.findByCredentials(body.email, body.password)
+    const token = await user.generateAuthToken()
+    res.header('x-auth', token).send(user)
+  } catch (err) {
+    if (NODE_ENV == 'development') {console.log(err.message)}
+    res.status(400).send()
+  }
 })
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token)
-    .then(()=> {
-      res.status(200).send()
-    })
-    .catch((err) => {
-      if (NODE_ENV == 'development') {console.log(err.message)}
-      res.status(400).send()
-    })
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token)
+    res.status(200).send()
+  } catch(err) {
+    if (NODE_ENV == 'development') {console.log(err.message)}
+    res.status(400).send()
+  }
 })
 
 app.listen(PORT, () => {
